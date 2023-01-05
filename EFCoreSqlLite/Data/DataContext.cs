48e0fe00709e2869fdde3b0f1f58e3e1ead54cef
Melihcan Serbest <canmelih.free3@gmail.com>
2023-01-05 18:05:04 +0300

    Add project files.

---
+++ EFCoreSqlLite/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreSqlLite.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<RpgCharacter> RpgCharacter => Set<RpgCharacter>();
+    }
+}
